2a9f48c7f1d151c243bdb04d611727ffe8368ffd
Michael Durando <mjdurando82@yahoo.com>
2023-01-23 16:56:13 -0500

    jump implemented

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 ///GLOBAL VARIABLES
-let score = 0
+let score = document.querySelector('.score')
 const mango = document.querySelector('.game-piece')
 const rock = document.querySelector('.rock')
-let playing = true
-///GAME LOGIC/// FUNCTIONS
+let gameOver = false
+///GAME LOGIC & FUNCTIONS
+
+let bottom = 0
 
 const jump = () => {
+  bottom += 40
+  mango.style.bottom = bottom + 'px'
+
   console.log('jump')
 }
+
 const motion = () => {}
+
+// const endGame = () => {
+//   if(rock pos === mango pos){
+//     gameOver = true
+//   }
+// }
 
 ///EVENT LISTENERS
 
-document.addEventListener('keyup', () => {
+document.addEventListener('keydown', () => {
   if (event.code === 'Space') {
     jump()
-    motion()
   }
 })
