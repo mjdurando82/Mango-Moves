8a1f63d625c1edd2db422a53e39384abbae4c487
Michael Durando <mjdurando82@yahoo.com>
2023-01-24 10:26:09 -0500

    rock has constant motion

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let gameOver = false
 const jump = () => {
   let bottom = 0
   let jumpUpTimer = setInterval(() => {
-    if (bottom > 125) {
+    if (bottom > 115) {
       clearInterval(jumpUpTimer)
       let jumpDownTimer = setInterval(() => {
         if (bottom <= 0) {
@@ -19,28 +19,26 @@ const jump = () => {
         mango.style.bottom = bottom + 'px'
       }, 40)
     }
-    bottom += 40
+    bottom += 30
     mango.style.bottom = bottom + 'px'
   }, 40)
 }
 const rockMotion = () => {
   let right = 0
   let leftTimer = setInterval(() => {
-    if (right > 675) {
-      clearInterval(leftTimer)
-    } else right += 60
+    if (right > 680) {
+      right = 0
+    } else right += 55
     rock.style.right = right + 'px'
-  }, 200)
+  }, 100)
 }
 if (gameOver === false) {
   rockMotion()
 }
-// const endGame = () => {
-//   if(rock pos === mango pos){
-//     gameOver = true
-//   }
-// }
-
+if (mango.style.bottom < '85px' && rock.style.right > '545px') {
+  gameOver = true
+  clearInterval(leftTimer)
+}
 ///EVENT LISTENERS
 
 document.addEventListener('keydown', () => {
