60cde2e685c61da8d0baad8e9f57ff0b15319132
Michael Durando <mjdurando82@yahoo.com>
2023-01-24 16:23:28 -0500

    changes from px to vw

--- script.js
+++ script.js
@@ -3,11 +3,13 @@
 let scoreDisplay = document.querySelector('.score-int')
 let mango = document.querySelector('.game-piece')
 let rock = document.querySelector('.rock')
-const ball = document.querySelector('.ball')
+let ball = document.querySelector('.ball')
 let gameOver = false
 const gameIsOver = document.querySelector('.game-over')
 let score = 0
+
 ///GAME LOGIC & FUNCTIONS
+
 const jump = () => {
   let bottom = 0
   let jumpUpTimer = setInterval(() => {
@@ -30,29 +32,44 @@ const jump = () => {
 const rockMotion = () => {
   let right = 0
   let leftTimer = setInterval(() => {
-    if (right > 690) {
+    if (right > 99) {
       right = 0
-    } else right += 50
-    rock.style.right = right + 'px'
+    } else right += 5
+    rock.style.right = right + 'vw'
     rock = document.querySelector('.rock')
-    // console.log(rock.style.right)
-    // console.log(mango.style.bottom)
-    if (rock.style.right >= '550px' && mango.style.bottom <= '0px') {
+    console.log(rock.style.right)
+    console.log(mango.style.bottom)
+    if (rock.style.right >= '90vw' && mango.style.bottom <= '0vh') {
       gameOver = true
       clearInterval(leftTimer)
       gameIsOver.style.opacity = 1
     }
   }, 100)
 }
+// const ballMotion = () => {
+//   let right = 0
+//   let ballTimer = setInterval(() => {
+//     if (right > 690) {
+//       right = 0
+//     } else right += 75
+//     ball.style.right = right + 'px'
+//     ball = document.querySelector('.ball')
+//     if (ball.style.right >= '550px' && mango.style.bottom >= '75px') {
+//       score += 25
+//       console.log('hit')
+//       clearInterval(ballTimer)
+//       ball.style.opacity = 0
+//     }
+//   }, 150)
+// }
 if (gameOver === false) {
   rockMotion()
+  // ballMotion()
 }
 
 const increaseScore = () => {
-  if (gameOver === false) {
-    score += 10
-    scoreDisplay.innerText = score
-  }
+  score += 10
+  scoreDisplay.innerText = score
 }
 
 ///EVENT LISTENERS
